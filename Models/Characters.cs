cfddc7e747f8f7584847ec90eba9a4842c2c487c
Aloysius <aloysiuspacheco2003@gmail.com>
2025-02-10 03:46:09 +0000

    Add project files.

---
+++ Models/Characters.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace ZenlessZoneZeroCharacterAPI.Models
+{
+    public class Characters
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string? Name { get; set; }
+
+        //zenlesszonezero charater types 
+        [Required]
+        [RegularExpression("^(Anomaly|Attack|Defence|Stun|Support)$", ErrorMessage = "Invalid Type. Type Should be of type Anomaly, Attack, Defence, Stun, Support")]
+        public string? Type { get; set; }
+
+        //zenlesszonezero charater elements
+        [Required]
+        [RegularExpression("^(Electric|Ether||Fire|Ice|Physical)$", ErrorMessage = "Invalid Element Element Should be of type Electric, Ether, Fire, Ice, Physical")]
+        public String? Element { get; set; }
+
+        [Required]
+        public int? Heath { get; set; }
+
+        [Required]
+        public int? Damage { get; set; }
+
+        public List<Items>? Items { get; set; }
+    }
+}
---
+++ Models/Items.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ZenlessZoneZeroCharacterAPI.Models
+{
+    public class Items
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string? Name { get; set; }
+
+        [Required]
+        [RegularExpression("^(Weapon|Accessory)$", ErrorMessage = "Invalid Type. Type Should be of type Weapon, Accessory")]
+        public string? Type { get; set; }
+
+        public int? AddedDamage { get; set; }
+
+        public int? AddedHealth { get; set; }
+
+        [Required]
+        [RegularExpression("^(Health|Damage)$", ErrorMessage = "Invalid Bonus Type. Type Should be of type Health, Damage")]
+        public int? BonusType { get; set; }
+
+        [Required]
+        public int? BonusValue { get; set; }
+    }
+}
